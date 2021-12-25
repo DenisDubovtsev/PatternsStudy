2ec7a0f6d5bffc69d022c037623c498a54b84821
DenisDubovtsev <77612500+DenisDubovtsev@users.noreply.github.com>
2021-12-25 19:58:47 +0600

    initial commit

---
+++ PatternsStudy/PatternsStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternsStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
