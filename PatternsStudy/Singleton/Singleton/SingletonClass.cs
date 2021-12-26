616a88f2012371be7b8494b8e3bad26dcfa7cd28
DenisDubovtsev <77612500+DenisDubovtsev@users.noreply.github.com>
2021-12-26 11:14:07 +0600

    first variant of singleton

---
+++ PatternsStudy/Singleton/Singleton/SingletonClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Singleton.Singleton
+{
+    public sealed class SingletonClass
+    {
+        private static SingletonClass _instance;
+        private SingletonClass() { }
+
+        public static SingletonClass GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new SingletonClass();
+            }
+
+            return _instance;
+        }
+
+        public static void SomeBusinessLogic()
+        {
+            Console.WriteLine("I am a singleton!");
+        }
+    }
+}
